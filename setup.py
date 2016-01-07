929d02971c53f557a5e55fb10864701c64a18a5e
radzhome <radzhome@gmail.com>
2016-01-06 19:08:10 -0500

    update setup params

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
-from setuptools import setup, find_packages
+from setuptools import setup  # , find_packages
+import emailage
+
+LONG_DESCRIPTION = open('README.md').read()
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+KEYWORDS = 'emailAge api wrapper'
 
 setup(
-    name='awseb-fab-tasks',
-    packages=find_packages(),
-    install_requires=['requests', ],
+    name='emailage',
+    # packages=find_packages(),
     include_package_data=True,
-    version='0.0.1',
-    description='Python EmailAge Wrapper',
-    long_description='A python wrapper for EmailAge API',
-    author='radlws',
-    url='http://www.github.com/radlws',
+    version=emailage.__version__,
+    description='A Python EmailAge API Wrapper',
+    long_description=LONG_DESCRIPTION,
+    author='radzhome',
+    download_url='http://pypi.python.org/pypi/emailage/',
+    url='http://www.github.com/radzhome',
+    packages=['emailage', ],
+    package_dir={'emailage': 'emailage'},
+    platforms=['Platform Independent'],
     license='BSD',
-    zip_safe=False,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    requires=['requests'],
+    install_requires=['requests'],
 )
